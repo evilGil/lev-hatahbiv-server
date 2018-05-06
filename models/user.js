eb2ea594b2b9262647250ae2503d3847adb95cc5
evilGil <gilkahan@gmail.com>
2018-05-06 12:08:48 +0300

    add validations to user schema

--- models/user.js
+++ models/user.js
@@ -4,9 +4,25 @@ var Schema = mongoose.Schema;
 
 var UserSchema = new Schema(
     {
-        user_name: { type: String, required: true, max: 100 },
-        email: { type: String, required: true, max: 100, unique: true },
-        password: { type: String, required: true, max: 100 },
+        user_name: {
+            type: String,
+            required: [true, "must specify user name"],
+            maxlength: [10, "user name is too long"],
+            minlength: [3, "user name is too short"]
+        },
+        email: {
+            type: String,
+            required: [true, "must specify email"],
+            maxlength: [100, "email is too long"],
+            minlength: [3, "email is too short"],
+            unique: true
+        },
+        password: {
+            type: String,
+            required: [true, "must specify password"],
+            maxlength: [100, "password is too long"],
+            minlength: [3, "password is too short"]
+        },
     }
 );
 
